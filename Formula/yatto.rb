ddcdcaeadbc17f1766bc9848de5b3c60621c6012
Benedikt Zumtobel <benedikt@zumtobel.dev>
2025-10-02 11:12:42 +0200

    Update formula to v0.19.4

--- Formula/yatto.rb
+++ Formula/yatto.rb
@@ -2,8 +2,8 @@ class Yatto < Formula
   desc "Interactive VCS-based todo-list for the command-line"
   homepage "https://github.com/handlebargh/yatto"
   url "https://github.com/handlebargh/yatto.git",
-      tag:      "v0.19.3",
-      revision: "bc4bce650f2f9dcf06c15c40b4abb5a21536600b"
+      tag:      "v0.19.4",
+      revision: "20bc6627d66eb5aece0da184c20c0609fcb458b5"
   license "MIT"
 
   depends_on "go" => :build
